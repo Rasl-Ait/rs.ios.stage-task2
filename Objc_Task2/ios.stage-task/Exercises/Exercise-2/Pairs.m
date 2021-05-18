b4b2d47e87ff3b7d134f5f508803b5e70807f056
Rasl-Ait <dovshi1208@gmail.com>
2021-05-18 16:03:11 +0300

    feat: finish exercises

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,19 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+	if (array.count < 2)
+	{
+			return 0;
+	}
+	int counter = 0;
+	for (NSUInteger i = 0; i < array.count; i++) {
+			for (NSUInteger j = i+1; j< array.count; j++) {
+					if ([array objectAtIndex:j].intValue - [array objectAtIndex:i].intValue == number.intValue) {
+							counter++;
+					}
+			}
+	}
+	return counter;
 }
 
 @end
